2052c8b16c78a5ef7c7e9a3534d102e32ea3654d
Federico Giorgino <f_giorgino@icloud.com>
2021-09-22 16:16:45 +0200

    Theme Context

---
+++ src/context/Theme.context.tsx
@@ -0,0 +1,36 @@
+import { ExtendedTheme } from "@react-navigation/native";
+import React, { useState, createContext, FC } from "react";
+import { CustomDarkTheme, CustomDefaultTheme } from "../theme/index";
+
+type ThemeContextState = {
+  isDarkTheme: boolean;
+  theme: ExtendedTheme;
+  switchTheme: () => void;
+};
+
+const contextDefaultValue: ThemeContextState = {
+  isDarkTheme: true,
+  theme: CustomDarkTheme,
+  switchTheme: () => {},
+};
+
+export const ThemeContext =
+  createContext<ThemeContextState>(contextDefaultValue);
+
+export const ThemeContextProvider: FC = ({ children }) => {
+  const [isDarkTheme, setIsDarkTheme] = useState<boolean>(
+    contextDefaultValue.isDarkTheme
+  );
+
+  const theme = isDarkTheme ? CustomDarkTheme : CustomDefaultTheme;
+
+  const switchTheme = () => {
+    setIsDarkTheme((isDarkTheme) => !isDarkTheme);
+  };
+
+  return (
+    <ThemeContext.Provider value={{ isDarkTheme, theme, switchTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
